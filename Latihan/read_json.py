4f20d39f23434e000148c24d997ca555e1567826
RachmadHi <16520115@std.stei.itb.ac.id>
2022-09-28 07:40:07 +0700

    update

--- Latihan/read_json.py
+++ Latihan/read_json.py
@@ -11,7 +11,8 @@ with open("data_mahasiswa.json", "r") as r_file :
 async def read_mahasiswa (NIM : str):
     for mahasiswa_item in data["data_mahasiswa"] :
         if mahasiswa_item["NIM"] == NIM :
-            return mahasiswa_item["Nama"]
+            return mahasiswa_item
     raise HTTPException(
         status_code = 404, detail = f"Data not found"
     )
+
